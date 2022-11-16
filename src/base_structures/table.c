11ec7090bb7eb42f8cb326609910d0f1da58a692
Rudolf Copi Eckelberg <rudolfce@gmail.com>
2022-11-15 21:24:38 -0300

    ADD: Table initialization operations

--- src/base_structures/table.c
+++ src/base_structures/table.c
@@ -1,8 +1,101 @@
 #include<stdio.h>
 #include<malloc.h>
+#include<string.h>
 #include "../includes/table.h"
 #include "../includes/page_operations.h"
 
+
+table_struct *initialize_table(char *name)
+{
+    table_struct *output_table;
+    int name_len;
+
+    output_table = (table_struct *) malloc(sizeof(table_struct));
+    name_len = strlen(name);
+    output_table->name = (char *) malloc((name_len + 1) * sizeof(char));
+    strcpy(output_table->name, name);
+    output_table->max_columns = 0;
+    output_table->n_columns = 0;
+    output_table->max_loaded_pages = 0;
+    output_table->n_loaded_pages = 0;
+    output_table->t_columns = NULL;
+    output_table->column_names = NULL;
+    output_table->loaded_pages = NULL;
+
+    return output_table;
+}
+
+int free_table(table_struct *table)
+{
+    int i;
+
+    if(!table)
+        return 0;
+
+    if(table->name)
+    {
+        free(table->name);
+    }
+    if(table->t_columns)
+    {
+        free(table->t_columns);
+    }
+    if(table->column_names)
+    {
+        for(i=0; i<table->n_columns; i++)
+        {
+            if(table->column_names)
+                free(table->column_names[i]);
+        }
+        free(table->column_names);
+    }
+    if(table->loaded_pages)
+    {
+        for(i=0; i<table->n_loaded_pages; i++)
+        {
+            free_page(table->loaded_pages[i]);
+        }
+        free(table->loaded_pages);
+    }
+
+    free(table);
+    return 1;
+}
+
+int resize_columns_vectors(table_struct *table, int new_size)
+{
+    if(!table->t_columns) {
+        table->t_columns = (int *) malloc(new_size * sizeof(int));
+    } else {
+        table->t_columns = (int *) realloc(table->t_columns, new_size * sizeof(int));
+    }
+    if(!table->column_names) {
+        table->column_names = (char **) malloc(new_size * sizeof(char **));
+    } else {
+        table->column_names = (char **) realloc(table->column_names, new_size * sizeof(char **));
+    }
+
+    return new_size;
+}
+
+int add_column(table_struct *table, char *column_name, int column_type)
+{
+    int last_index;
+    int l_name;
+
+    if(table->n_columns == table->max_columns)
+        table->max_columns = resize_columns_vectors(table, table->max_columns + COLUMNS_INCREMENT);
+
+    last_index = table->n_columns;
+    table->n_columns += 1;
+
+    l_name = strlen(column_name);
+    table->column_names[last_index] = (char *) malloc((l_name + 1) * sizeof(char));
+    strcpy(table->column_names[last_index], column_name);
+    table->t_columns[last_index] = column_type;
+
+    return table->n_columns;
+}
 
 int get_row_size(table_struct *table)
 {
--- src/includes/table.h
+++ src/includes/table.h
@@ -3,15 +3,18 @@
 
 #define TYPE_INTEGER 1
 #define TYPE_VARCHAR 2
+#define COLUMNS_INCREMENT 16
 
 /* A table defined by name, size and column types.
    A page cache vector is provided to avoid loading
    pages too many times. */
 typedef struct table_struct{
+    int max_columns;
     int *t_columns;
     char **column_names;
     int n_columns;
     int n_rows;
+    int max_loaded_pages;
     int n_loaded_pages;
     struct page_struct **loaded_pages;
     char *name;
@@ -20,12 +23,15 @@ typedef struct table_struct{
 
 typedef struct page_struct{
     struct table_struct *table;
-    int dirty;
+    char dirty;
     int page_number;
     int n_rows;
     char *page_start;
 } page_struct;
 
+table_struct *initialize_table(char *);
+int free_table(table_struct *);
+int add_column(table_struct *, char *, int);
 page_struct *load_page(table_struct *, int);
 int free_page(page_struct *);
 int get_row_size(table_struct *);
